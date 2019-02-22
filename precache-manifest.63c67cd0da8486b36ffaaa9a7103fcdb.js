7a63a75537aeda16bbc252272890cf2fae976ed3
Eric Allen <eric.justin.allen@gmail.com>
2019-02-22 15:08:08 -0600

    Updates

---
+++ precache-manifest.63c67cd0da8486b36ffaaa9a7103fcdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70fc1c39704dec08e9c9",
+    "url": "/test-react/static/css/main.af11fd35.chunk.css"
+  },
+  {
+    "revision": "70fc1c39704dec08e9c9",
+    "url": "/test-react/static/js/main.70fc1c39.chunk.js"
+  },
+  {
+    "revision": "0e319f87483090a0303e",
+    "url": "/test-react/static/js/runtime~main.0e319f87.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/test-react/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "90dc73fba7866749d6de14634acf537b",
+    "url": "/test-react/index.html"
+  }
+];
